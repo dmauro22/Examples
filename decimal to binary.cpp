63c0e38e517403f02e33fb4de01adfc2af38a6c3
Daniel Mauro <33274156+dmauro22@users.noreply.github.com>
2017-12-16 10:17:53 -0600

    Add files via upload

---
+++ decimal to binary.cpp
@@ -0,0 +1,91 @@
+//*******************************************************
+// Decimal to Binary conversion:
+//
+// This program prompts user to input an integer, then
+// calculates and returns the binary form of the integer
+// 
+//
+//*******************************************************
+
+#include <iostream>			// for cin, cout, endl
+#include <iomanip>          // to format the data 
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+int spacesNeeded(int);
+void binaryCalc(int[], int, int);
+
+int main()
+{
+	int theNumber;			// input number
+	int *theBinary = NULL;	// binary equivalent
+	int places;				// number of array slots needed
+	char cont = 'y';		// Continue flag
+	bool negFlag;			// Flag for negative number
+
+	while (cont == 'y' || cont == 'Y')
+	{
+		theNumber = 0;
+		places = 0;
+		cout << "Enter a positive integer: ";
+		cin >> theNumber;
+
+
+		places = spacesNeeded(theNumber);
+
+		theBinary = new int[places];
+
+		binaryCalc(theBinary, places, theNumber);
+
+		// Special case if input number is zero
+		if (theNumber == 0)
+		{
+			places = 1;
+			theBinary[0] = 0;
+		}
+
+		cout << "Binary form of " << theNumber << " is ";
+		for (int i = 0; i < places; i++)
+			cout << theBinary[i];
+		cout << endl << endl << "Again?  Y for yes: ";
+		cin >> cont;
+		cout << endl;
+	}
+
+	delete[] theBinary;
+	theBinary = nullptr;
+
+	system("pause");
+	return 0;
+}
+
+// Function that determines the number spaces needed to hold the binary number
+int spacesNeeded(int mainNum)
+{
+	int slots = 0;
+	//int incrementer = 0;
+	while ((mainNum - pow(2, slots) + 1) > 0)
+	{
+		slots++;
+	}
+	return slots;
+}
+
+// Function to converts the decimal number to binary
+void binaryCalc(int theBinary[], int places, int theNumber)
+{
+	for (int i = 0; i < places; i++)
+	{
+		if ((theNumber % int(pow(2, i + 1))) == 0)
+		{
+			theBinary[places - 1 - i] = 0;
+		}
+		else
+		{
+			theBinary[places - 1 - i] = 1;
+			theNumber -= pow(2, i);
+		}
+	}
+}
