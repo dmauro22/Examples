47634ef63ae89f831fc3f400abf6c4b7bbfa1f99
Daniel Mauro <33274156+dmauro22@users.noreply.github.com>
2017-12-20 22:37:10 -0600

    Create integer_math.cpp

---
+++ integer_math.cpp
@@ -0,0 +1,147 @@
+//******************************************************************
+// COSC 1337: Lab 1
+// Programmer: Daniel Mauro
+// Completed : Sept 19, 2017
+// Status    : Complete
+//
+// This program will be used to start my project
+//******************************************************************
+
+#include <iostream>				// for cin, cout, endl
+
+using namespace std;
+
+// Declare function prototypes
+
+int sumnum(int, int);
+
+int prodnum(int, int);
+
+int avgnum(int, int);
+
+int divnum(int, int);
+
+int modnum(int, int);
+
+int main()	
+					
+{ 
+// Declare variables
+
+	int num1 = 0;
+	
+	int num2 = 0;
+	
+	int sums = 0;
+
+	int prod = 0;
+	
+	int avg = 0;
+	
+	int div = 0;
+	
+	int mod = 0;
+	
+// Get values for first two integers
+
+	cout << "Enter any integer: ";
+	
+	cin >> num1;
+
+	cout << "Enter another integer: ";
+	
+	cin >> num2;
+
+// Calculate sum of integers
+
+	sums = sumnum(num1, num2);
+
+// Calculate product of integers
+	
+	prod = prodnum(num1, num2);
+	
+// Calculate average of integers
+
+	avg = avgnum(num1, num2);
+	
+// Calculate integer 1 divided by integer 2
+
+	div = divnum(num1, num2);
+	
+// Calculate remainder of integer 1 divided by integer 2
+
+	mod = modnum(num1, num2);
+	
+// Print results of calculations
+
+	cout << "Integer 1 = " << num1 << endl;
+	
+	cout << "Integer 2 = " << num2 << endl;
+	
+	cout << "The sum = " << sums << endl;
+	
+	cout << "The product = " << prod << endl;
+
+	cout << "The average = " << avg << endl;
+		
+	cout << "The quotient = " << div << endl;
+
+	cout << "The modulus = " << mod << endl;
+	
+	return 0;
+	
+
+}
+
+int sumnum(int num1, int num2)
+
+{
+	
+	int sums = num1 + num2;
+	
+	return sums;
+
+}
+
+
+int prodnum(int num1, int num2)
+
+{
+	
+	int prod = num1 * num2;
+	
+	return prod;
+
+}
+
+
+int avgnum(int num1, int num2)
+
+{
+	
+	int avg = (num1 + num2) / 2;
+	
+	return avg;
+
+}
+
+int divnum(int num1, int num2)
+
+{
+	
+	int div = num1 / num2;
+	
+	return div;
+
+}
+
+int modnum(int num1, int num2)
+
+{
+	
+	int mod = num1 % num2;
+	
+	return mod;
+
+}
+
