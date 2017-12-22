a450b547d832376498ff9fd385eba8dcb0f0dee6
Daniel Mauro <33274156+dmauro22@users.noreply.github.com>
2017-12-21 22:33:20 -0600

    Update Baum_Sweet.cpp

--- Baum_Sweet.cpp
+++ Baum_Sweet.cpp
@@ -1,4 +1,4 @@
-///******************************************************************
+//******************************************************************
 // Baum Sweet Sequence
 //
 // Prompts user to enter a positive integer and calculates 
@@ -15,9 +15,8 @@ using namespace std;
 int main()
 {
 	// Variable declarations
-
-	string baum = "1101";		// First 4 elements of Baum-Sweet sequence
-	int baseNum;			// number to calculate up to
+	string baum = "1101";
+	int baseNum;				// Number to calculate sequence to
 	int i = 2;				// array element marker
 
 	cout << "Enter base nummber: ";
@@ -26,25 +25,18 @@ int main()
 	// Calculate Baum-Sweet sequence
 	while (i < baseNum)
 	{
-		if (baum[i] == '0')
-			if (baum[i + 1] == '0')
-			{
-				baum.append("0000");
-			}
+		if (baum[i] == '0')			
+			if (baum[i + 1] == '0')    
+				baum.append("0000");	// 00 yields 0000
 			else
-			{
-				baum.append("1001");
-			}
+				baum.append("1001");	// 01 yields 1001
+
 		else
 			if (baum[i + 1] == '0')
-			{
-				baum.append("0100");
-			}
+				baum.append("0100");	// 10 yields 0100
 			else
-			{
-				baum.append("1101");
-			}
-		i += 2;
+				baum.append("1101");	// 11 yields 1101
+		i += 2;					// Move to the next two elements
 	}
 
 	// Output results
@@ -57,4 +49,3 @@ int main()
 
 	return 0;
 }
-
